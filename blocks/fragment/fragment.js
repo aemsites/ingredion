6cf87a27f12a2822c0f6b0389fd4c77df9040c0e
Dave Fink <138233107+dave-fink@users.noreply.github.com>
2024-11-25 13:54:28 -0500

    1 header (#34)
    * header WIP
    
    * header WIP
    
    * caps fix
    
    * stylint comment
    
    * Solid state before mobile view
    
    * good state
    
    * fixed mobile view

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -17,7 +17,7 @@ import {
  * @param {string} path The path to the fragment
  * @returns {HTMLElement} The root element of the fragment
  */
-export async function loadFragment(path) {
+export async function loadFragment(path, decorateBlock = true) {
   if (path && path.startsWith('/')) {
     const resp = await fetch(`${path}.plain.html`);
     if (resp.ok) {
@@ -33,7 +33,7 @@ export async function loadFragment(path) {
       resetAttributeBase('img', 'src');
       resetAttributeBase('source', 'srcset');
 
-      decorateMain(main);
+      if (decorateBlock) decorateMain(main);
       await loadSections(main);
       return main;
     }
