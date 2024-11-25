30fd315b35e6c0861120fabcb5c48e562fe4daf8
Varun Mitra <vmitra@adobe.com>
2024-11-25 15:48:57 -0500

    commit_1

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,15 +1,31 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
+  /* change to ul, li */  
+  const ul = document.createElement('ul');  
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+    while (row.firstElementChild) li.append(row.firstElementChild);   
+    [...li.children].forEach((div) => {        
+      if (div.children.length === 1 && div.querySelector('picture')) {
+         div.className = 'cards-card-image';
+         const imageAnchor = document.createElement('a');
+         const picture = div.querySelector('picture');
+         imageAnchor.append(picture);
+         div.append(imageAnchor);
+         } else div.className = 'cards-card-body';      
     });
+    const btnContainer = li.querySelector('.button-container');
+    if (btnContainer) {
+      btnContainer.classList.remove('button-container');
+      const a = btnContainer.querySelector('a');
+      const span = document.createElement('span');
+      span.className = 'icon-green-arrow';
+      a.append(span);
+      const imageAnchor = li.querySelector('.cards-card-image a');
+      imageAnchor.href = a.href;
+      imageAnchor.setAttribute('aria-label', a.href);
+  }
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
