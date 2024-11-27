f79ec53cd1defc9efac340bd6753dcfba9c01c37
ujjgupta <ujjgupta@adobe.com>
2024-11-27 18:43:24 +0530

    Commenting OOTB code instead of deleting

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,5 @@
 import {
+  /* buildBlock, */
   loadHeader,
   loadFooter,
   decorateButtons,
@@ -13,6 +14,23 @@ import {
 } from './aem.js';
 
 /**
+ * Builds hero block and prepends to main in a new section.
+ * @param {Element} main The container element
+ */
+/*
+function buildHeroBlock(main) {
+  const h1 = main.querySelector('h1');
+  const picture = main.querySelector('picture');
+  // eslint-disable-next-line no-bitwise
+  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+    const section = document.createElement('div');
+    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    main.prepend(section);
+  }
+}
+*/
+
+/**
  * load fonts.css and set a session storage flag
  */
 async function loadFonts() {
@@ -25,6 +43,21 @@ async function loadFonts() {
 }
 
 /**
+ * Builds all synthetic blocks in a container element.
+ * @param {Element} main The container element
+ */
+/*
+function buildAutoBlocks(main) {
+  try {
+    buildHeroBlock(main);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Auto Blocking failed', error);
+  }
+}
+*/
+
+/**
  * Decorates the main element.
  * @param {Element} main The main element
  */
@@ -33,6 +66,7 @@ export function decorateMain(main) {
   // hopefully forward compatible button decoration
   decorateButtons(main);
   decorateIcons(main);
+  // buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
 }
