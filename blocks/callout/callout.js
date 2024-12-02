c88554fd5512963d545cfe43ac41e3b4f5956d90
Giorgia Fiscaletti <giorgiafiscaletti@gmail.com>
2024-12-02 18:08:05 +0100

    fix textWrapper

--- blocks/callout/callout.js
+++ blocks/callout/callout.js
@@ -2,10 +2,18 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   const pic = block.querySelector('picture');
-  const title = block.querySelector('h3');
+  const h3 = block.querySelector('h3');
+  const h1 = block.querySelector('h1');
   const link = block.querySelector('a');
 
-  const textWrapper = title.closest('div');
+  let textWrapper;
+
+  if (h1) {
+    textWrapper = h1.closest('div');
+  } else if (h3) {
+    textWrapper = h3.closest('div');
+  }
+
   textWrapper.classList.add('callout-content');
 
   if (!block.classList.contains('transparent')) {
