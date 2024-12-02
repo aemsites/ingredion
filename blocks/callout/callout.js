5a49c382fd9b0ebce6dfb2fca5e94016b35d5d4f
Giorgia Fiscaletti <giorgiafiscaletti@gmail.com>
2024-12-02 16:37:30 +0100

    Callout blocks (#40)
    * callout image block, padding work in progress
    
    * update style of callout block with image
    
    * fix lint
    
    * fix comma
    
    * remove unused code
    
    * set margin:auto for content
    
    * adjust scopes
    
    * button: remove redundant statements, fix hover
    
    * change color on button hover
    
    * fix button color and hover
    
    * update font families
    
    * adjust paddings and margins
    
    * adjust wrapper width
    
    * fix height when <1080px
    
    * adjust mobile view
    
    * change font size to rem
    
    * fix lint problems
    
    * add style for ul and li
    
    * add dark purple theme
    
    * create text-only callout
    
    * center text blocks and button in callout-content
    
    * fix lint errors
    
    * adjust flext for callout div
    
    * restore package.json and package-lock.json

---
+++ blocks/callout/callout.js
@@ -0,0 +1,28 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
+
+export default function decorate(block) {
+  const pic = block.querySelector('picture');
+  const title = block.querySelector('h3');
+  const link = block.querySelector('a');
+
+  const textWrapper = title.closest('div');
+  textWrapper.classList.add('callout-content');
+
+  link.classList.add('transparent');
+
+  if (pic) {
+    const picWrapper = pic.closest('div');
+    picWrapper.classList.add('callout-image');
+
+    const img = pic.querySelector('img');
+    const optimizedPicture = createOptimizedPicture(
+      img.src,
+      img.alt,
+      false,
+      [{ width: '750' }],
+    );
+    pic.replaceWith(optimizedPicture);
+  } else {
+    block.classList.add('text-only');
+  }
+}
