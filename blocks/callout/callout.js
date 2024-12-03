153974283924402a05f00ad9a79e8c3467378a9e
Giorgia Fiscaletti <giorgiafiscaletti@gmail.com>
2024-12-03 12:10:55 +0100

    Issue #17.2: add transparent variant for callout block (#48)
    * add transparent variant
    
    * fix textWrapper
    
    * adjust callout top margin
    
    * fix wrapper margin
    
    * change margin to one-liner

--- blocks/callout/callout.js
+++ blocks/callout/callout.js
@@ -2,13 +2,23 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   const pic = block.querySelector('picture');
-  const title = block.querySelector('h3');
+  const h3 = block.querySelector('h3');
+  const h1 = block.querySelector('h1');
   const link = block.querySelector('a');
 
-  const textWrapper = title.closest('div');
+  let textWrapper;
+
+  if (h1) {
+    textWrapper = h1.closest('div');
+  } else if (h3) {
+    textWrapper = h3.closest('div');
+  }
+
   textWrapper.classList.add('callout-content');
 
-  link.classList.add('transparent');
+  if (!block.classList.contains('transparent')) {
+    link.classList.add('transparent');
+  }
 
   if (pic) {
     const picWrapper = pic.closest('div');
