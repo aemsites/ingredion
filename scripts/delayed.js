d56a67e3f02f2b57b7b8d7ade54dec2144189dfd
Anurag Singh <anurags9693@gmail.com>
2024-12-04 23:50:02 +0500

    Back To Top Button

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,31 @@
 // add delayed functionality here
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const backToTopButton = document.createElement("button");
+    backToTopButton.id = "backToTop";
+    backToTopButton.textContent = "↑";
+  
+    
+    document.body.appendChild(backToTopButton);
+  
+    window.addEventListener("scroll", () => {
+      const scrollY = window.scrollY; 
+      const windowHeight = window.innerHeight; 
+      const documentHeight = document.documentElement.scrollHeight; 
+  
+      // button will appear when user hit x amount of scroll
+      if (scrollY > 300 && documentHeight > windowHeight + 300) {
+        backToTopButton.classList.add("show");
+      } else {
+        backToTopButton.classList.remove("show");
+      }
+    });
+  
+    //for smooth scroll
+    backToTopButton.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+      });
+    });
+  });
+  
