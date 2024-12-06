9f93ffae61b276a21313fb93912693e5f08fbd41
dave-fink <david@keypixel.com>
2024-12-06 16:33:17 -0500

    Cleaning up styles

--- templates/news-detail/news-detail.js
+++ templates/news-detail/news-detail.js
@@ -1,6 +1,7 @@
 /* eslint-disable function-paren-newline, object-curly-newline */
 import { nav, div, h2, a, img } from '../../scripts/dom-helpers.js';
 import { getMetadata } from '../../scripts/aem.js';
+import { breadcrumbs } from '../../scripts/breadcrumbs.js';
 
 export default function decorate(doc) {
   const $main = doc.querySelector('main');
@@ -15,25 +16,7 @@ export default function decorate(doc) {
   const type = getMetadata('type');
   const publishedDate = getMetadata('published-date');
   const categories = getMetadata('categories');
-
-  const $breadcrumbs = nav({ class: 'breadcrumbs' });
-  // todo: update breadcrumbs - static HTML for now
-  $breadcrumbs.innerHTML = `
-    <ul>
-      <li>
-        <a href="/na/en-us.html">Ingredion <span class="icon-arrow-blk"></span></a>
-      </li>
-      <li>
-        <a href="/na/en-us/news-events.html">News and events <span class="icon-arrow-blk"></span></a>
-      </li>
-      <li>
-        <a href="/na/en-us/news-events/news.html">News <span class="icon-arrow-blk"></span></a>
-      </li>
-      <li>
-        <b>Ingredion Launches VITESSENCE Pea 200 D, Shaking Up Drinking Experiences in Nutritional Beverages</b>
-      </li>
-    </ul>
-  `;
+  const $breadcrumbs = breadcrumbs();
 
   const $header = div({ class: 'header' },
     $breadcrumbs,
@@ -47,7 +30,7 @@ export default function decorate(doc) {
     div({ class: 'description' }, teaserDescription),
     div({ class: 'social-share' },
       a({
-        class: 'icon-linkedin',
+        class: 'icon icon-linkedin',
         href: 'https://www.linkedin.com/cws/share?url=https://www.ingredion.com/content/ingredion-com/na/en-us/news-events/news/Pea-protein-improved-dispersibility-RTM-beverages.html',
         'aria-label': 'LinkedIn' },
       img({ src: '/icons/linkedin.svg', alt: 'LinkedIn' }),
