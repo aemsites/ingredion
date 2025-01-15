fbea1f39bcfcfb379b7c40c68ddbe271575703ad
Varun Mitra <vmitra@adobe.com>
2025-01-15 15:50:38 -0500

    updating code as per PR comments

--- blocks/anchor/anchor.js
+++ blocks/anchor/anchor.js
@@ -2,15 +2,4 @@ export default function decorate(block) {
   [...block.querySelectorAll('a.button')].forEach((button) => {
     button.classList.remove('button');
   });
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      console.log(entry);
-      const intersecting = entry.isIntersecting;
-      if (!intersecting) {
-        block.classList.add('is-visible');
-        console.log('not-visible');       
-      }      
-    });
-  });
-  observer.observe(block);
 }
