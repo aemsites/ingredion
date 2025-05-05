e8183a68eed60cbcb4bbcc67de2afd888dab4486
Aliaksandra Kupreyeva <akupreyeva@adobe.com>
2025-05-05 14:45:16 +0200

    styling fixes

--- blocks/header-dropdown/header-dropdown.js
+++ blocks/header-dropdown/header-dropdown.js
@@ -1,53 +1,52 @@
 /* eslint-disable function-paren-newline, object-curly-newline */
-import { div, ul, li } from "../../scripts/dom-helpers.js";
-import { createOptimizedPicture } from "../../scripts/aem.js";
+import { div, ul, li, span } from '../../scripts/dom-helpers.js';
+import { createOptimizedPicture } from '../../scripts/aem.js';
 
 function buildSubMenu(block) {
-  const $navList = ul({ class: "nav-list", "data-height": "" });
-  const $navItems = div({ class: "nav-items" });
+  const $navList = ul({ class: 'nav-list', 'data-height': '' });
+  const $navItems = div({ class: 'nav-items' });
 
   const removeActiveItem = () => {
-    [...$navItems.children].forEach((item) => item.classList.remove("active"));
-    [...$navList.children].forEach((listItem) =>
-      listItem.classList.remove("active"),
+    [...$navItems.children].forEach((item) => item.classList.remove('active'));
+    [...$navList.children].forEach((listItem) => listItem.classList.remove('active'),
     );
   };
 
   const setActiveItem = (row, rowN) => {
     removeActiveItem();
-    row.classList.add("active");
-    const matchingItem = $navItems.querySelector(`.item[data-item="${rowN}"]`);
-    if (matchingItem) matchingItem.classList.add("active");
+    row.classList.add('active');
+    const matchingItem = $navItems.querySelector(`.item[data-item='${rowN}']`);
+    if (matchingItem) matchingItem.classList.add('active');
   };
 
   [...block.children].forEach((row, rowN) => {
-    const $item = div({ class: "item", "data-item": rowN });
+    const $item = div({ class: 'item', 'data-item': rowN });
 
     // set first item as active
-    if (rowN === 0) $item.classList.add("active");
+    if (rowN === 0) $item.classList.add('active');
 
     [...row.children].forEach((col, colN) => {
       // replace image with optimized version
-      const img = col.querySelector("img");
+      const img = col.querySelector('img');
       if (img) {
         const newImg = createOptimizedPicture(
           img.src,
-          img.alt || "image",
+          img.alt || 'image',
           false,
-          [{ width: "400" }],
+          [{ width: '400' }],
         );
         img.replaceWith(newImg);
       }
 
       if (colN === 0) {
         // build section list
-        const $section = li({ "data-item": rowN }, col.textContent);
-        $section.addEventListener("click", () => setActiveItem($section, rowN));
+        const $section = li({ 'data-item': rowN }, col.textContent);
+        $section.addEventListener('click', () => setActiveItem($section, rowN));
         // set first item as active
-        if (rowN === 0) $section.classList.add("active");
+        if (rowN === 0) $section.classList.add('active');
         $navList.append($section);
       } else {
-        col.setAttribute("data-height", "");
+        col.setAttribute('data-height', '');
         $item.append(col);
       }
     });
@@ -55,23 +54,23 @@ function buildSubMenu(block) {
     $navItems.append($item);
   });
 
-  block.innerHTML = "";
+  block.innerHTML = '';
   block.append($navList, $navItems);
 }
 
 function buldStaticDropdown(block) {
-  block.classList.add("static");
+  block.classList.add('static');
 
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
-      col.setAttribute("data-height", "");
-      const img = col.querySelector("img");
+      col.setAttribute('data-height', '');
+      const img = col.querySelector('img');
       if (img) {
         const newImg = createOptimizedPicture(
           img.src,
-          img.alt || "image",
+          img.alt || 'image',
           false,
-          [{ width: "400" }],
+          [{ width: '400' }],
         );
         img.replaceWith(newImg);
       }
@@ -83,30 +82,29 @@ function renderDesktop(block) {
   block.classList.add(
     `columns-${[...block.firstElementChild.children].length}`,
   );
-  if (block.classList.contains("submenu")) buildSubMenu(block);
+  if (block.classList.contains('submenu')) buildSubMenu(block);
   else buldStaticDropdown(block);
 }
 
 function renderMobile(block) {
-  block.classList.remove("submenu");
-  block.classList.add("mobile");
-  const dropdowns = block.querySelectorAll("div > div");
+  block.classList.remove('submenu');
+  block.classList.add('mobile');
+  const dropdowns = block.querySelectorAll('div > div');
 
   dropdowns.forEach((dropdown) => {
-    dropdown.querySelectorAll("img, picture").forEach((el) => el.remove());
-    dropdown.querySelectorAll("p").forEach((p) => {
-      if (p.querySelector("a")) {
-        const div = document.createElement("div");
-        div.innerHTML = p.innerHTML;
-        div.className = p.className;
+    dropdown.querySelectorAll('img, picture').forEach((el) => el.remove());
+    dropdown.querySelectorAll('p').forEach((p) => {
+      if (p.querySelector('a')) {
+        const dropdownDiv = div({ class: p.className });
+        dropdownDiv.innerHTML = p.innerHTML;
         [...p.attributes].forEach((attr) => {
-          if (attr.name !== "class") div.setAttribute(attr.name, attr.value);
+          if (attr.name !== 'class') dropdownDiv.setAttribute(attr.name, attr.value);
         });
-        p.replaceWith(div);
+        p.replaceWith(dropdownDiv);
       }
     });
 
-    dropdown.querySelectorAll("p").forEach((p) => {
+    dropdown.querySelectorAll('p').forEach((p) => {
       const hasVisibleText = p.textContent.trim().length > 0;
       const hasOtherContent = [...p.children].length > 0;
       if (!hasVisibleText && !hasOtherContent) {
@@ -114,26 +112,25 @@ function renderMobile(block) {
       }
     });
 
-    dropdown.querySelectorAll("div").forEach((div) => {
-      const hasVisibleText = div.textContent.trim().length > 0;
-      const hasOtherContent = [...div.children].length > 0;
+    dropdown.querySelectorAll('div').forEach((dropdownDiv) => {
+      const hasVisibleText = dropdownDiv.textContent.trim().length > 0;
+      const hasOtherContent = [...dropdownDiv.children].length > 0;
       if (!hasVisibleText && !hasOtherContent) {
-        div.remove();
+        dropdownDiv.remove();
       }
     });
 
     let title = null;
-    dropdown.querySelectorAll("p").forEach((p) => {
+    dropdown.querySelectorAll('p').forEach((p) => {
       if (!title && p.textContent.trim().length > 0) {
         title = p;
 
         const parent = p.parentElement;
         const grandparent = parent.parentElement;
 
-        const isWrapped =
-          parent.tagName === "DIV" &&
-          parent.children.length === 1 &&
-          parent.contains(p);
+        const isWrapped = parent.tagName === 'DIV'
+          && parent.children.length === 1
+          && parent.contains(p);
         if (isWrapped && grandparent) {
           grandparent.insertBefore(p, parent);
           parent.remove();
@@ -143,37 +140,43 @@ function renderMobile(block) {
 
     if (!title) return;
 
-    title.classList.add("dropdown-title");
-    title.classList.remove("open");
+    title.classList.add('dropdown-title');
+    title.classList.remove('open');
+
+    if (!title.querySelector('.icon-add') && !title.querySelector('.icon-subtract')) {
+      title.appendChild(span({ class: 'icon-add open' }));
+      title.appendChild(span({ class: 'icon-subtract' }));
+    }
 
     const dropdownItems = [...title.parentElement.children].filter(
       (el) => el !== title,
     );
 
     dropdownItems.forEach((dropdownItem) => {
-      dropdownItem.classList.add("dropdown-content");
-      dropdownItem.classList.remove("open");
+      dropdownItem.classList.add('dropdown-content');
+      dropdownItem.classList.remove('open');
 
-      dropdownItem.addEventListener("click", (event) => {
-        //event.stopPropagation();
-        dropdownItem.classList.toggle("open");
+      dropdownItem.addEventListener('click', () => {
+        dropdownItem.classList.toggle('open');
       });
     });
 
     let isOpen = false;
 
-    title.addEventListener("click", () => {
+    title.addEventListener('click', () => {
       isOpen = !isOpen;
-      dropdownItems.forEach((sibling) => {
-        sibling.classList.toggle("open", isOpen);
+      dropdownItems.forEach((dropdownItem) => {
+        dropdownItem.classList.toggle('open', isOpen);
       });
+      title.querySelector('.icon-add').classList.toggle('open', !isOpen);
+      title.querySelector('.icon-subtract').classList.toggle('open', isOpen);
     });
   });
 }
 
 export default function decorate(block) {
-  const isMobile = window.matchMedia("(width < 1080px)");
-  const getMode = () => (isMobile.matches ? "mobile" : "desktop");
+  const isMobile = window.matchMedia('(width < 1080px)');
+  const getMode = () => (isMobile.matches ? 'mobile' : 'desktop');
 
   const originalHTML = block.innerHTML;
   const originalClasses = [...block.classList];
@@ -181,14 +184,14 @@ export default function decorate(block) {
 
   function resetBlock(html, classes) {
     block.innerHTML = html;
-    block.className = "";
+    block.className = '';
     block.classList.add(...classes);
   }
 
   function applyLayout(mode) {
     resetBlock(originalHTML, originalClasses);
 
-    if (mode === "mobile") {
+    if (mode === 'mobile') {
       renderMobile(block);
     } else {
       renderDesktop(block);
@@ -199,8 +202,8 @@ export default function decorate(block) {
 
   applyLayout(currentMode);
 
-  if (!block._hasResizeListener) {
-    isMobile.addEventListener("change", () => {
+  if (!block.hasResizeListener) {
+    isMobile.addEventListener('change', () => {
       const newMode = getMode();
       if (newMode !== currentMode) {
         currentMode = newMode;
@@ -208,6 +211,6 @@ export default function decorate(block) {
       }
     });
 
-    block._hasResizeListener = true;
+    block.hasResizeListener = true;
   }
 }
--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,24 +1,46 @@
 /* eslint-disable function-paren-newline, object-curly-newline */
 import { loadFragment } from '../fragment/fragment.js';
-import { div, nav, span, img, form, input, button, a } from '../../scripts/dom-helpers.js';
+import {
+  div,
+  nav,
+  span,
+  img,
+  form,
+  input,
+  button,
+  a,
+} from '../../scripts/dom-helpers.js';
 import { getCookie, getRegionLocale, throttle } from '../../scripts/utils.js';
 
 const isMobile = window.matchMedia('(width < 1080px)');
 const [region, locale] = getRegionLocale();
-const $originalLogo = a({ class: 'logo', href: `/${region}/${locale}/`, 'aria-label': 'Home' },
-    img({ src: '/icons/ingredion.svg', width: 120, alt: 'Ingredion logo' }),
-    );
+const $originalLogo = a(
+  { class: 'logo', href: `/${region}/${locale}/`, 'aria-label': 'Home' },
+  img({ src: '/icons/ingredion.svg', width: 120, alt: 'Ingredion logo' }),
+);
 
 function resetDropdownsMobile($header) {
-  $header.querySelectorAll(".dropdown").forEach((dropdown) => {
-    dropdown.style.display = "none";
+  $header.querySelectorAll('.category .dropdown').forEach((dropdown) => {
+    dropdown.style.display = 'none';
 
     const container = dropdown.parentElement;
-    container.style.display = "block";
-    container.classList.remove("active");
+    container.style.display = 'block';
+    container.classList.remove('active');
 
-    dropdown.querySelectorAll(".dropdown-content.open").forEach((el) => {
-      el.classList.remove("open");
+    dropdown.querySelectorAll('.dropdown-content.open').forEach((el) => {
+      el.classList.remove('open');
+    });
+
+    container.querySelectorAll('.view-all').forEach((el) => {
+      el.classList.remove('active');
+    });
+
+    document.querySelectorAll('.icon-add').forEach((el) => {
+      el.classList.add('open');
+    });
+
+    document.querySelectorAll('.icon-subtract').forEach((el) => {
+      el.classList.remove('open');
     });
   });
 }
@@ -29,12 +51,13 @@ function setDropdownHeights($header) {
     dropdown.style.visibility = 'hidden';
     dropdown.style.position = 'absolute';
 
-    const heights = Array.from(dropdown.querySelectorAll('[data-height]'))
-      .map((el) => {
+    const heights = Array.from(dropdown.querySelectorAll('[data-height]')).map(
+      (el) => {
         const height = el.clientHeight;
         el.removeAttribute('data-height');
         return height;
-      });
+      },
+    );
 
     let maxHeight = Math.max(0, ...heights);
     const $row = dropdown.querySelector('.section.row');
@@ -53,66 +76,52 @@ async function buildDropdownsDesktop($header) {
   let activeDropdown = null;
 
   async function attachDropdown(link) {
-    const subNavPath = link.getAttribute("href");
-    // remove to prevent click action and from being shown in the browser
-    link.removeAttribute("href");
-    link.setAttribute("data-dropdown", "true");
+    const subNavPath = link.getAttribute('href');
+    link.removeAttribute('href');
+    link.setAttribute('data-dropdown', 'true');
 
-    // Load fragment and append dropdown content
     const subNavFrag = await loadFragment(subNavPath);
     if (!subNavFrag) {
       link.remove();
       return;
     }
-    const $dropDown = div({ class: "dropdown" });
-    while (subNavFrag.firstElementChild)
-      $dropDown.append(subNavFrag.firstElementChild);
+    const $dropDown = div({ class: 'dropdown' });
+    while (subNavFrag.firstElementChild) $dropDown.append(subNavFrag.firstElementChild);
     link.parentElement.append($dropDown);
 
     const openDropdown = throttle(
       () => {
         if (activeDropdown && activeDropdown !== $dropDown) {
-          activeDropdown.parentElement.classList.remove("active");
+          activeDropdown.parentElement.classList.remove('active');
         }
-        $dropDown.parentElement.classList.add("active");
+        $dropDown.parentElement.classList.add('active');
         activeDropdown = $dropDown;
       },
       100,
       140,
-    ); // small delay to prevent unintentional events
+    );
 
-    link.addEventListener("pointerenter", openDropdown);
+    link.addEventListener('pointerenter', openDropdown);
   }
 
-  // load dropdowns attach event listeners in parallel
   const dropdownPromise = links.map(attachDropdown);
 
-  // close dropdown if clicked outside
   document.addEventListener(
-    "click",
+    'click',
     (event) => {
-      if (
-        activeDropdown &&
-        !activeDropdown.contains(event.target) &&
-        !event.target.closest("a[data-dropdown]")
-      ) {
-        activeDropdown.parentElement.classList.remove("active");
+      if (activeDropdown && !activeDropdown.contains(event.target) && !event.target.closest('a[data-dropdown]')) {
+        activeDropdown.parentElement.classList.remove('active');
         activeDropdown = null;
       }
     },
     true,
   );
 
-  // close dropdown on pointer leave
   document.addEventListener(
-    "pointerleave",
+    'pointerleave',
     (event) => {
-      if (
-        activeDropdown &&
-        !activeDropdown.contains(event.target) &&
-        !event.target.closest("a[data-dropdown]")
-      ) {
-        activeDropdown.parentElement.classList.remove("active");
+      if (activeDropdown && !activeDropdown.contains(event.target) && !event.target.closest('a[data-dropdown]')) {
+        activeDropdown.parentElement.classList.remove('active');
         activeDropdown = null;
       }
     },
@@ -122,41 +131,45 @@ async function buildDropdownsDesktop($header) {
   await Promise.all(dropdownPromise).then(() => {
     requestAnimationFrame(() => {
       setDropdownHeights($header);
-      $header.classList.add("loaded");
+      $header.classList.add('loaded');
     });
   });
 }
 
 async function buildDropdownsMobile($header) {
   const links = [...$header.querySelectorAll('a[href*="/dropdowns"]')];
-  let activeDropdown = null;
-
-  const logo = document.querySelector(".logo");
-  const utility = document.querySelector(".utility");
-  const btnTechDocSamples = document.querySelector(".btn-tech-doc-samples");
+  const utility = document.querySelector('.utility');
+  const btnTechDocSamples = document.querySelector('.btn-tech-doc-samples');
 
   const backButton = a(
-    { class: "back-button" },
-    span({ class: "icon-green-arrow-up" }),
-    span({ class: "back-text" }, "BACK"),
+    { class: 'back-button' },
+    span({ class: 'icon-green-arrow-up' }),
+    span({ class: 'back-text' }, 'BACK'),
   );
 
-  backButton.addEventListener("click", () => {
-    // Restore utility and tech doc samples button
-    utility.style.display = "block";
-    btnTechDocSamples.style.display = "block";
+  backButton.addEventListener('click', () => {
+    utility.style.display = 'block';
+    btnTechDocSamples.style.display = 'block';
 
     resetDropdownsMobile($header);
-    activeDropdown = null;
 
-    const btnContainer = document.querySelector(".btn-container");
+    const btnContainer = document.querySelector('.btn-container');
     btnContainer.replaceChildren($originalLogo);
   });
 
   async function attachDropdown(link) {
-    const subNavPath = link.getAttribute("href");
-    link.removeAttribute("href");
-    link.setAttribute("data-dropdown", "true");
+    const subNavPath = link.getAttribute('href');
+    link.removeAttribute('href');
+    link.setAttribute('data-dropdown', 'true');
+
+    const spanWrapper = span({});
+    while (link.firstChild) {
+      spanWrapper.appendChild(link.firstChild);
+    }
+    link.appendChild(spanWrapper);
+
+    const viewAllButton = span({ class: 'view-all' }, 'VIEW ALL', span({ class: 'icon-green-arrow-up' }));
+    link.appendChild(viewAllButton);
 
     const subNavFrag = await loadFragment(subNavPath);
     if (!subNavFrag) {
@@ -164,46 +177,47 @@ async function buildDropdownsMobile($header) {
       return;
     }
 
-    const $dropDown = div({ class: "dropdown" });
+    const $dropDown = div({ class: 'dropdown' });
     while (subNavFrag.firstElementChild) {
       $dropDown.append(subNavFrag.firstElementChild);
     }
 
     link.parentElement.append($dropDown);
-    let openDropdown;
 
-    openDropdown = throttle(
+    const openDropdown = throttle(
       () => {
-        // Hide other dropdowns
-        $header.querySelectorAll(".dropdown").forEach((dropdown) => {
+        $header.querySelectorAll('.dropdown').forEach((dropdown) => {
           if (dropdown !== $dropDown) {
-            dropdown.parentElement.classList.remove("active");
-            dropdown.parentElement.style.display = "none";
+            dropdown.parentElement.classList.remove('active');
+            dropdown.parentElement.style.display = 'none';
           }
         });
 
-        $dropDown.style.display = "block";
-        $dropDown.parentElement.classList.add("active");
-        activeDropdown = $dropDown;
+        $dropDown.style.display = 'block';
+        $dropDown.parentElement.classList.add('active');
+        viewAllButton.classList.add('active');
 
-        const btnContainer = document.querySelector(".btn-container");
+        const btnContainer = document.querySelector('.btn-container');
         btnContainer.replaceChildren(backButton);
 
-        utility.style.display = "none";
-        btnTechDocSamples.style.display = "none";
+        if (utility.style.display === 'none') {
+          utility.style.display = 'block';
+        } else {
+          utility.style.display = 'none';
+        }
+        btnTechDocSamples.style.display = 'none';
       },
       100,
       140,
     );
 
-    link.addEventListener("click", openDropdown);
+    link.addEventListener('click', openDropdown);
   }
 
   const dropdownPromise = links.map(attachDropdown);
   await Promise.all(dropdownPromise).then(() => {
     requestAnimationFrame(() => {
-      setDropdownHeights($header);
-      $header.classList.add("loaded");
+      $header.classList.add('loaded');
     });
   });
 }
@@ -213,73 +227,103 @@ export default async function decorate(block) {
   const navPath = `/${region}/${locale}/header/header`;
   const navFrag = await loadFragment(navPath, false);
   const navSections = navFrag.querySelectorAll('main > div');
-  const $utilityLinks = Array.from(navSections[0].querySelectorAll(':scope > *'));
-  const $btnTechDocSamples = div({ class: 'btn-tech-doc-samples' }, navSections[1].querySelector(':scope > p > a'));
-  const $categoryNav = Array.from(navSections[2].querySelectorAll(':scope > *'));
+  const $utilityLinks = Array.from(
+    navSections[0].querySelectorAll(':scope > *'),
+  );
+  const $btnTechDocSamples = div(
+    { class: 'btn-tech-doc-samples' },
+    navSections[1].querySelector(':scope > p > a'),
+  );
+  const $categoryNav = Array.from(
+    navSections[2].querySelectorAll(':scope > *'),
+  );
 
   const $header = document.querySelector('header');
 
-  const $btnCart = a({ class: 'icon-cart', href: `/${region}/${locale}/sample-cart`, 'aria-label': 'Cart' },
+  const $btnCart = a(
+    {
+      class: 'icon-cart',
+      href: `/${region}/${locale}/sample-cart`,
+      'aria-label': 'Cart',
+    },
     '\u{e919}',
-    span({ class: 'count' }, (() => {
-      const cartCookies = getCookie('cartCookies');
-      if (!cartCookies || cartCookies.split('cookie ').length === 0) {
-        return '0';
-      }
-      return (cartCookies.split('cookie ').length - 1).toString();
-    })()),
+    span(
+      { class: 'count' },
+      (() => {
+        const cartCookies = getCookie('cartCookies');
+        if (!cartCookies || cartCookies.split('cookie ').length === 0) {
+          return '0';
+        }
+        return (cartCookies.split('cookie ').length - 1).toString();
+      })(),
+    ),
   );
 
   if ($btnCart.querySelector('.count').textContent === '0') {
     $btnCart.querySelector('.count').classList.add('hide');
   }
 
-  const $btnBurger = button({ class: "icon-burger", "aria-label": "Menu" });
-  $btnBurger.addEventListener("click", () => {
-    document.body.classList.toggle("menu-open");
-    
+  const $btnBurger = button({ class: 'icon-burger', 'aria-label': 'Menu' });
+  $btnBurger.addEventListener('click', () => {
+    document.body.classList.toggle('menu-open');
+
     resetDropdownsMobile($header);
 
-    const btnContainer = document.querySelector(".btn-container");
-    const hasBackButton = btnContainer.querySelector(".back-button");
+    const btnContainer = document.querySelector('.btn-container');
+    const hasBackButton = btnContainer.querySelector('.back-button');
     if (hasBackButton) {
       btnContainer.replaceChildren($originalLogo);
     }
   });
 
-  const $searchBar = div({ class: 'search-bar' },
-    form({ class: 'search', id: 'searchForm' },
-      div({ class: 'search-box' },
-        div({ class: 'category select-dropdown' },
+  const $searchBar = div(
+    { class: 'search-bar' },
+    form(
+      { class: 'search', id: 'searchForm' },
+      div(
+        { class: 'search-box' },
+        div(
+          { class: 'category select-dropdown' },
           div({ class: 'selected' }, 'All'),
         ),
         input({ id: 'search', 'aria-label': 'Search Input' }),
-        button({ type: 'submit', class: 'icon-search', 'aria-label': 'Search Button' }),
+        button({
+          type: 'submit',
+          class: 'icon-search',
+          'aria-label': 'Search Button',
+        }),
       ),
-      button({ type: 'submit', form: 'searchForm', class: 'button-search', 'aria-label': 'Search Button' }, 'Search'),
+      button(
+        {
+          type: 'submit',
+          form: 'searchForm',
+          class: 'button-search',
+          'aria-label': 'Search Button',
+        },
+        'Search',
+      ),
     ),
   );
 
   const $navCategory = nav({ class: 'category' }, ...$categoryNav);
 
-  // change view for dektop or mobile
   async function handleView() {
-    $header.innerHTML = "";
+    $header.innerHTML = '';
     if (isMobile.matches) {
       $header.append(
         div(
-          { class: "logo-cart-burger-wrap" },
-          div({ class: "btn-container" }, $originalLogo.cloneNode(true)),
+          { class: 'logo-cart-burger-wrap' },
+          div({ class: 'btn-container' }, $originalLogo.cloneNode(true)),
           $btnCart.cloneNode(true),
           $btnBurger,
         ),
         $searchBar.cloneNode(true),
         div(
-          { class: "mobile-menu" },
+          { class: 'mobile-menu' },
           $navCategory.cloneNode(true),
           $btnTechDocSamples.cloneNode(true),
           nav(
-            { class: "utility" },
+            { class: 'utility' },
             ...$utilityLinks.map((link) => link.cloneNode(true)),
           ),
         ),
@@ -288,13 +332,13 @@ export default async function decorate(block) {
     } else {
       $header.append(
         nav(
-          { class: "utility" },
+          { class: 'utility' },
           ...$utilityLinks.map((link) => link.cloneNode(true)),
           $btnCart.cloneNode(true),
         ),
         div(
-          { class: "logo-search-btn-wrap" },
-          div({ class: "btn-container" }, $originalLogo.cloneNode(true)),
+          { class: 'logo-search-btn-wrap' },
+          div({ class: 'btn-container' }, $originalLogo.cloneNode(true)),
           $searchBar.cloneNode(true),
           $btnTechDocSamples.cloneNode(true),
         ),
@@ -303,6 +347,7 @@ export default async function decorate(block) {
       await buildDropdownsDesktop($header);
     }
   }
+
   handleView();
   isMobile.addEventListener('change', handleView);
 }
