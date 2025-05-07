999202c22e180e8deb69aef8abdb93992b831abe
dave-fink <david@keypixel.com>
2025-05-07 14:39:55 -0400

    lint fixes

--- blocks/article-list/article-list.js
+++ blocks/article-list/article-list.js
@@ -21,7 +21,6 @@ export default async function decorate(block) {
 
   let $articlePage;
 
-
   if (block.classList.contains('cards')) {
     // cards view
     const $filterYearsDropdown = div();
@@ -79,77 +78,75 @@ export default async function decorate(block) {
       perPageDropdown: $perPageDropdown,
       countDiv: $count,
     }).render();
-  } else if (block.classList.contains('events')) {  
-        // Events view
-        const $filterYearsDropdown = div();
-        const $filterTypesDropdown = div();
-        const $clearFilters = a({ class: 'clear-all' }, 'Clear All');
-    
-        const $articleCard = (article) => div({ class: 'card events' },
-          div({ class: 'image-wrapper' },
-            div({ class: 'thumb' },
-              p({ class: 'type' }, JSON.parse(article.eventType)),
-              createOptimizedPicture(article.image, article.title, true, [{ width: '235' }]),
-            ),
+  } else if (block.classList.contains('events')) {
+    // Events view
+    const $filterYearsDropdown = div();
+    const $filterTypesDropdown = div();
+    const $clearFilters = a({ class: 'clear-all' }, 'Clear All');
+
+    const $articleCard = (article) => div({ class: 'card events' },
+      div({ class: 'image-wrapper' },
+        div({ class: 'thumb' },
+          p({ class: 'type' }, JSON.parse(article.eventType)),
+          createOptimizedPicture(article.image, article.title, true, [{ width: '235' }]),
+        ),
+      ),
+      div({ class: 'info' },
+        p({ class: 'date' }, parseEventDate(article.eventDate, true)),
+        h4(article.title),
+        article.eventType && JSON.parse(article.eventType).length ? p({ class: 'details' }, strong('Event Type: '), JSON.parse(article.eventType)) : null,
+        article.location && JSON.parse(article.location).length ? p({ class: 'details' }, strong('Location: '), JSON.parse(article.location)) : null,
+        article.boothNumber && JSON.parse(article.boothNumber).length ? p({ class: 'details' }, strong('Booth Number: '), JSON.parse(article.boothNumber)) : null,
+        div({ class: 'description' }, JSON.parse(article.content)),
+      ),
+      div({ class: 'buttons' },
+        (() => {
+          if (article.watchNow && JSON.parse(article.watchNow).length) {
+            return a({ class: 'button', href: JSON.parse(article.watchNow) }, 'Watch Now');
+          } if (article.registrationEventSite && JSON.parse(article.registrationEventSite).length) {
+            return a({ class: 'button', href: JSON.parse(article.registrationEventSite) }, 'Register');
+          }
+          return null;
+        })(),
+        a({ class: 'button secondary', href: article.path }, 'Go To Event Site'),
+      ),
+    );
+
+    $articlePage = div({ class: 'article-list' },
+      div({ class: 'filter-search-sort' },
+        $search,
+        $filterYearsDropdown,
+        $filterTypesDropdown,
+        $sortDropdown,
+      ),
+      div({ class: 'clear-all-wrapper' }, $clearFilters),
+      div({ class: 'filter-results-wrapper' },
+        div({ class: 'results events' },
+          $count,
+          $articles,
+          div({ class: 'controls' },
+            $pagination,
+            $perPageDropdown,
           ),
-          div({ class: 'info' },
-            p({ class: 'date' }, parseEventDate(article.eventDate, true)),
-            h4(article.title),
-            article.eventType && JSON.parse(article.eventType).length ? p({ class: 'details' }, strong('Event Type: '), JSON.parse(article.eventType)) : null,
-            article.location && JSON.parse(article.location).length ? p({ class: 'details' }, strong('Location: '), JSON.parse(article.location)) : null,
-            article.boothNumber && JSON.parse(article.boothNumber).length ? p({ class: 'details' }, strong('Booth Number: '), JSON.parse(article.boothNumber)) : null,
-            div({ class: 'description' }, JSON.parse(article.content)),
-          ),
-          div({ class: 'buttons' },
-            (() => {
-              if (article.watchNow && JSON.parse(article.watchNow).length) {
-                return a({ class: 'button', href: JSON.parse(article.watchNow) }, 'Watch Now');
-              } if (article.registrationEventSite && JSON.parse(article.registrationEventSite).length) {
-                return a({ class: 'button', href: JSON.parse(article.registrationEventSite) }, 'Register');
-              }
-              return null;
-            })(),
-            a({ class: 'button secondary', href: article.path }, 'Go To Event Site'),
-          ),
-        );
-    
-        $articlePage = div({ class: 'article-list' },
-          div({ class: 'filter-search-sort' },
-            $search,
-            $filterYearsDropdown,
-            $filterTypesDropdown,
-            $sortDropdown,
-          ),
-          div({ class: 'clear-all-wrapper' }, $clearFilters),
-          div({ class: 'filter-results-wrapper' },
-            div({ class: 'results events' },
-              $count,
-              $articles,
-              div({ class: 'controls' },
-                $pagination,
-                $perPageDropdown,
-              ),
-            ),
-          ),
-        );
-    
-        await new ArticleRenderer({
-          jsonPath,
-          articlesPerPageOptions,
-          paginationMaxBtns,
-          articleDiv: $articles,
-          articleCard: $articleCard,
-          clearFilters: $clearFilters,
-          filterYearsDropdown: $filterYearsDropdown,
-          filterTypesDropdown: $filterTypesDropdown,
-          searchDiv: $search,
-          sortDropdown: $sortDropdown,
-          paginationDiv: $pagination,
-          perPageDropdown: $perPageDropdown,
-          countDiv: $count,
-        }).render();
-  
-  
+        ),
+      ),
+    );
+
+    await new ArticleRenderer({
+      jsonPath,
+      articlesPerPageOptions,
+      paginationMaxBtns,
+      articleDiv: $articles,
+      articleCard: $articleCard,
+      clearFilters: $clearFilters,
+      filterYearsDropdown: $filterYearsDropdown,
+      filterTypesDropdown: $filterTypesDropdown,
+      searchDiv: $search,
+      sortDropdown: $sortDropdown,
+      paginationDiv: $pagination,
+      perPageDropdown: $perPageDropdown,
+      countDiv: $count,
+    }).render();
   } else {
     // list view
     const $filtersList = div();
