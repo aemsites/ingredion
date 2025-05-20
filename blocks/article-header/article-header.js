9fcad972a2ecf8f3339015998ea7e3f9cde022ea
Dave Fink <138233107+dave-fink@users.noreply.github.com>
2025-05-20 16:21:46 -0400

    Refactor article detail styles and JavaScript for improved structure … (#325)
    * Refactor article detail styles and JavaScript for improved structure and functionality
    
    - Simplified CSS by removing redundant styles and consolidating layout rules.
    - Updated JavaScript to streamline metadata handling and social share link generation.
    - Enhanced hero section image handling and added social share links to the article header.
    - Centered paragraphs marked with '###' for better alignment.
    
    * lint fixes

---
+++ blocks/article-header/article-header.js
@@ -0,0 +1,17 @@
+import { div, h2 } from '../../scripts/dom-helpers.js';
+
+export default function decorate(block) {
+  const newBlock = div();
+  [...block.children].forEach((row) => {
+    const [rowTitle, content] = [...row.children];
+    const cssClass = rowTitle.textContent.toLowerCase().replace(/\s+/g, '-');
+    if (content.textContent.trim()) {
+      if (cssClass === 'title') {
+        newBlock.append(h2(content.firstElementChild || content));
+      } else {
+        newBlock.append(div({ class: cssClass }, content.firstElementChild || content));
+      }
+    }
+  });
+  block.replaceWith(newBlock);
+}
