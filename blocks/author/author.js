1ce457127311bf913eaa35538b6d26a21dc205c7
Ujjwal Gupta <ujjwal5@users.noreply.github.com>
2025-05-23 22:12:21 +0530

    Modifying author block (#354)
    * Modifying author block
    
    * Modifying author block

--- blocks/author/author.js
+++ blocks/author/author.js
@@ -1,5 +1,5 @@
 /* eslint-disable function-paren-newline, object-curly-newline */
-import { div, p, h1, picture, img } from '../../scripts/dom-helpers.js';
+import { div, p, h1, h4, picture, img } from '../../scripts/dom-helpers.js';
 import { unwrapNestedDivs } from '../../scripts/scripts.js';
 
 const authorSpreadsheetPath = '/author/author-info.json';
@@ -9,12 +9,17 @@ export default async function decorate(block) {
 
   const resp = await fetch(authorSpreadsheetPath);
   const authorsJson = await resp.json();
-  const authorName = block.querySelector('p');
+  const authorName = block.innerText.trim();
   const authorInformation = authorsJson.data.find(
-    (item) => item.name === authorName.innerText,
+    (item) => item.name === authorName,
   );
 
-  authorName.remove();
+  const authorNameInBlock = block.querySelector('h4');
+  const isAuthorNameH4 = authorNameInBlock !== null;
+  block.innerHTML = '';
+  if (isAuthorNameH4) {
+    block.classList.add('static-author-block');
+  }
 
   if (authorInformation) {
     const authorImageContainer = div(
@@ -26,7 +31,8 @@ export default async function decorate(block) {
     );
     const authorTextContainer = div(
       { class: 'author-bio-text' },
-      div({ class: 'heading', tabIndex: 0 }, h1(authorInformation.name)),
+      div({ class: 'heading', tabIndex: 0 }, isAuthorNameH4 ? h4(authorInformation.name) : h1(authorInformation.name)),
+      authorInformation.designation && p({ class: 'designation', taxIndex: 0 }, authorInformation.designation),
       p({ class: 'body-text', taxIndex: 0 }, authorInformation.description),
     );
 
