16aa1e3ffeb982b239cfcbba04f57c845d42a23a
Varun Mitra <vmitra@adobe.com>
2025-06-05 12:54:33 -0400

    Update salesforce-chat-widget.js

--- scripts/salesforce-chat-widget.js
+++ scripts/salesforce-chat-widget.js
@@ -1,5 +1,7 @@
     import {loadScript} from './aem.js';    
-    await loadScript('./salesforce-init.js');
+
+(async () => {
+await loadScript('./salesforce-init.js');
     var initESW = function(gslbBaseURL) {
         embedded_svc.settings.displayHelpButton = true; //Or false
         embedded_svc.settings.language = ''; //For example, enter 'en' or 'en-US'
