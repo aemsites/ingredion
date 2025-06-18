364fa78a57a0a07eaaad8c1e15c1834147294511
shraddha <shraddha.s@ingredion.com>
2025-06-18 18:26:45 +0530

    lint fixes

--- blocks/offset-icons/offset-icons.js
+++ blocks/offset-icons/offset-icons.js
@@ -2,14 +2,16 @@ import { div } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
   const rows = block.querySelectorAll(':scope > div > div');
-  
+
   const offsetIconContainer = div({ class: 'offset-icons-list-wrapper' },
-    div({ class: 'offset-icons-overview' }, rows[0]),
-    div({ class: 'offset-icons-list' },
-      ...Array.from(rows).slice(1).map((row, i) => {
+    div(
+        { class: 'offset-icons-overview' }, rows[0]),
+    div(
+        { class: 'offset-icons-list' },
+      ...Array.from(rows).slice(1).map((row) => {
         const children = Array.from(row.children);
-        return div({ class: 'offset-icons-card'}, ...children);
-      })
+        return div({  class: 'offset-icons-card'}, ...children);
+      }),
     ),
   );
   block.replaceChildren(offsetIconContainer);
