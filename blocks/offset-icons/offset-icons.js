cf9b7bedabbea32620cd2bba768a09f6f65febf4
shraddha <shraddha.s@ingredion.com>
2025-06-25 19:44:50 +0530

    Lint fixes

--- blocks/offset-icons/offset-icons.js
+++ blocks/offset-icons/offset-icons.js
@@ -1,5 +1,34 @@
 import { div } from '../../scripts/dom-helpers.js';
 
+function createCard(element) {
+  const imageBlock = element[0];
+  const cardHeading = element[1];
+  const cardDescription = element[2];
+
+  const offsetIconCard = document.createElement('div');
+  offsetIconCard.classList.add('offset-icons-card');
+
+  const imageContainer = document.createElement('div');
+  imageContainer.classList.add('image-container');
+  if(imageBlock) {
+    imageContainer.append(imageBlock);
+  }
+
+  const contentBlock = document.createElement('div');
+  contentBlock.classList.add('content-block');
+  if(cardHeading) {
+    contentBlock.append(cardHeading);
+  }
+  if(cardDescription){
+    contentBlock.append(cardDescription);
+  }
+  
+  offsetIconCard.append(imageContainer);
+  offsetIconCard.append(contentBlock);
+
+  return offsetIconCard;
+}
+
 export default function decorate(block) {
   const rows = block.querySelectorAll(':scope > div > div');
 
@@ -15,27 +44,4 @@ export default function decorate(block) {
     ),
   );
   block.replaceChildren(offsetIconContainer);
- }
-
-function createCard(element) {
-  const imageBlock = element[0];
-  const cardHeading = element[1];
-  const cardDescription = element[2];
-
-  const offsetIconCard = document.createElement('div');
-  offsetIconCard.classList.add('offset-icons-card');
-  
-  const imageContainer = document.createElement('div');
-  imageContainer.classList.add('image-container');
-  imageBlock && imageContainer.append(imageBlock);
-
-  const contentBlock = document.createElement('div');
-  contentBlock.classList.add('content-block');
-  cardHeading && contentBlock.append(cardHeading);
-  cardDescription && contentBlock.append(cardDescription);
-  
-  offsetIconCard.append(imageContainer);
-  offsetIconCard.append(contentBlock);
-
-  return offsetIconCard;
 }
