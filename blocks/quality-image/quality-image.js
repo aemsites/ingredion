61ff226dd0b534aaa357e334dc3300657b5f4caf
shraddha <shraddha.s@ingredion.com>
2025-07-07 15:40:06 +0530

    quality image block changes

---
+++ blocks/quality-image/quality-image.js
@@ -0,0 +1,25 @@
+import { div } from '../../scripts/dom-helpers.js';
+import { unwrapNestedDivs } from '../../scripts/scripts.js';
+
+const widthConstant = [
+  'tiny',  
+  'small',
+  'medium',
+  ''
+];
+
+export default function decorate(block) {
+  unwrapNestedDivs(block);
+
+  const classListArray = Array.from(block.classList);
+  const imageWidth = widthConstant.find((width) => classListArray.includes(width));
+  const centerAlign = classListArray.includes('center') ? 'center-align': '';
+  const children = Array.from(block.children);
+  const imageContainer = div({ class: `section-width ${imageWidth} ${centerAlign}` });
+  
+  children.forEach((child) => {
+    imageContainer.append(child);
+  });
+
+  block.insertBefore(imageContainer, block.firstElementChild); 
+}
