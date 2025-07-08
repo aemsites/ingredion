507f5a38964689a7d4940ad27d4b97c5558d15d7
shraddha <shraddha.s@ingredion.com>
2025-07-08 16:22:15 +0530

    Lint fixes

--- blocks/quality-image/quality-image.js
+++ blocks/quality-image/quality-image.js
@@ -2,10 +2,10 @@ import { div } from '../../scripts/dom-helpers.js';
 import { unwrapNestedDivs } from '../../scripts/scripts.js';
 
 const widthConstant = [
-  'tiny',  
+  'tiny',
   'small',
   'medium',
-  ''
+  '',
 ];
 
 export default function decorate(block) {
@@ -13,13 +13,13 @@ export default function decorate(block) {
 
   const classListArray = Array.from(block.classList);
   const imageWidth = widthConstant.find((width) => classListArray.includes(width));
-  const centerAlign = classListArray.includes('center') ? 'center-align': '';
+  const centerAlign = classListArray.includes('center') ? 'center-align' : '';
   const children = Array.from(block.children);
   const imageContainer = div({ class: `section-width ${imageWidth} ${centerAlign}` });
-  
+
   children.forEach((child) => {
     imageContainer.append(child);
   });
 
-  block.insertBefore(imageContainer, block.firstElementChild); 
+  block.insertBefore(imageContainer, block.firstElementChild);
 }
