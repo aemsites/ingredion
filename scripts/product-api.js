8ed05eb0198c565d66b7031b8c86976d3e5e325c
shraddha <shraddha.s@ingredion.com>
2025-07-14 18:54:29 +0530

    host update

--- scripts/product-api.js
+++ scripts/product-api.js
@@ -1,10 +1,10 @@
 // This file contains all the product API endpoints
 
 // prod
-//export const API_HOST = 'https://www.ingredion.com';
+export const API_HOST = 'https://www.ingredion.com';
 
 // stage - testing (todo: remove before go-live)
- export const API_HOST = 'https://ingredion-stage65.adobecqms.net';
+//export const API_HOST = 'https://ingredion-stage65.adobecqms.net';
 
 export const getUrlParams = () => {
   const params = new URLSearchParams(window.location.search);
