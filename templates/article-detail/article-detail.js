552faa3b22124602776c896463fe7f96cc3af6c2
shraddha-s13 <shraddha.s@ingredion.com>
2025-08-11 15:51:10 +0530

    Article blog update (#627)
    * Article block css update
    
    * Article blog update
    
    * Updated line break
    
    * Lint error fix
    
    * Lint css error fix
    
    * Contact banner css update
    
    * author bio css updated
    
    * Removed comments

--- templates/article-detail/article-detail.js
+++ templates/article-detail/article-detail.js
@@ -89,4 +89,20 @@ export default async function decorate(doc) {
   });
 
   $main.prepend($hero);
+
+  // Add green line before author container
+  const $authorContainer = $main.querySelector('.author-wrapper');
+  if ($authorContainer) {
+    const $lineBrekEnd = div({ class: 'line-break-end' });
+    $authorContainer.prepend($lineBrekEnd);
+  }
+
+  // Move section with class name "outside-blog" outside of article blog section
+  const $outsideBlog = Array.from($main.querySelectorAll('.outside-blog'));
+  if ($outsideBlog.length) {
+    $outsideBlog.forEach((section) => {
+      $main.querySelector('.outside-blog').remove();
+      $main.append(section);
+    });
+  }
 }
