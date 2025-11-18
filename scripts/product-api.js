ce6af7e1941eea9b95a23304025482c02f613ced
Gayathri Anchal M K_C <gayathri.anchalmk@ingredion.com>
2025-11-18 17:43:33 +0530

    pointing to new andean sharepoint location

--- scripts/product-api.js
+++ scripts/product-api.js
@@ -35,46 +35,57 @@ export const DEFAULT_PATHS = {
 export const API_PATH_OVERRIDES = {
   POPULATE_INGREDIENT_CATEGORY_SUBCATEGORY: {
     'na-es-mx': '/bin/ingredion-com/headeringredient/header.es-mx.search.json',
+    'sa-es-co': '/bin/ingredion-com/headeringredient/header.es-co.search.json',
     'na-kerr': '/bin/ingredion-com/headeringredient/header.kerr.search.json',
   },
   SEARCH_INGREDIENT_BY_CATEGORY_SUBCATEGORY: {
     'na-es-mx': '/bin/ingredion-com/ingredient-finder.es-mx.search.json',
+    'sa-es-co': '/bin/ingredion-com/ingredient-finder.es-co.search.json',
     'na-kerr': '/bin/ingredion-com/ingredient-finder.kerr.search.json',
   },
   INGREDIENT_SEARCH_TYPEAHEAD: {
     'na-es-mx': '/bin/ingredion-com/typeaheadingredient.es-mx.ingredient-search-typeahead.json',
+    'sa-es-co': '/bin/ingredion-com/typeaheadingredient.es-co.ingredient-search-typeahead.json',
     'na-kerr': '/bin/ingredion-com/typeaheadingredient.kerr.ingredient-search-typeahead.json',
   },
   PRODUCT_DETAILS: {
     'na-es-mx': '/bin/ingredion-com/searchResults.es-mx.ingredients.json',
+    'sa-es-co': '/bin/ingredion-com/searchResults.es-co.ingredients.json',
     'na-kerr': '/bin/ingredion-com/searchResults.kerr.ingredients.json',
   },
   ALL_DOCUMENTS: {
     'na-es-mx': '/bin/ingredion-com/documents.es-mx.view.json',
+    'sa-es-co': '/bin/ingredion-com/documents.es-co.view.json',
     'na-kerr': '/bin/ingredion-com/documents.kerr.view.json',
   },
   DOWNLOAD_DOCUMENTS: {
     'na-es-mx': '/bin/ingredion-com/documents-download.es-mx.download.zip',
+    'sa-es-co': '/bin/ingredion-com/documents-download.es-co.download.zip',
     'na-kerr': '/bin/ingredion-com/documents-download.kerr.download.zip',
   },
   DOWNLOAD_ALL_DOCUMENTS: {
     'na-es-mx': '/bin/ingredion-com/documents-download.es-mx.download.zip',
+    'sa-es-co': '/bin/ingredion-com/documents-download.es-co.download.zip',
     'na-kerr': '/bin/ingredion-com/documents-download.kerr.download.zip',
   },
   DOWNLOAD_ALL_DOCUMENTS_FROM_SEARCH: {
     'na-es-mx': '/bin/ingredion-com/documents-download.es-mx.download.zip',
+    'sa-es-co': '/bin/ingredion-com/documents-download.es-co.download.zip',
     'na-kerr': '/bin/ingredion-com/documents-download.kerr.download.zip',
   },
   SEARCH_INGREDIENTS: {
     'na-es-mx': '/bin/ingredion-com/searchResults.es-mx.ingredients.json',
+    'sa-es-co': '/bin/ingredion-com/searchResults.es-co.ingredients.json',
     'na-kerr': '/bin/ingredion-com/searchResults.kerr.ingredients.json',
   },
   SEARCH_DOCUMENTS: {
     'na-es-mx': '/bin/ingredion-com/tech-documents.es-mx.techDocs.json',
+    'sa-es-co': '/bin/ingredion-com/tech-documents.es-co.techDocs.json',
     'na-kerr': '/bin/ingredion-com/tech-documents.kerr.techDocs.json',
   },
   SEARCH_INGREDIENTS_BY_NAME: {
     'na-es-mx': '/bin/ingredion-com/ingredient-finder.es-mx.search.json',
+    'sa-es-co': '/bin/ingredion-com/ingredient-finder.es-co.search.json',
     'na-kerr': '/bin/ingredion-com/ingredient-finder.kerr.search.json',
   },
 };
