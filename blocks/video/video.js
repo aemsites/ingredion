53d0f0786e5717c69ee07c0807efbaa60446e78a
Hariharasudhan Sargunam_C <hariharasudhan.sargunam@ingredion.com>
2026-09-08 15:46:27 +0530

    mute attribute updated

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -84,7 +84,7 @@ function getVideoElement(source, autoplay, background) {
     video.setAttribute('playsinline', '');
     video.removeAttribute('controls');
     video.addEventListener('canplay', () => {
-      video.muted = true;
+      video.muted = false;
       if (autoplay) video.play();
     });
   }
