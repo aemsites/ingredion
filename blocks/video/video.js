ff28023c529ab60858a73abba14997665b1a5c5f
Hariharasudhan Sargunam_C <hariharasudhan.sargunam@ingredion.com>
2026-09-22 12:43:36 +0530

    control conditions changed

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -32,7 +32,7 @@ function embedYoutube(url, autoplay, background) {
   if (background || autoplay) {
     const suffixParams = {
       autoplay: autoplay ? '1' : '0',
-      mute: background ? '1' : '0',
+      mute: background ? '0' : '1',
       controls: background ? '1' : '0',
       disablekb: background ? '1' : '0',
       loop: background ? '1' : '0',
